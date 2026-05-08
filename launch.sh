22650cc02bf133b7e43d6d06e5a9759fb13b1aa8
eak <eak@ivalua.com>
2026-05-08 04:24:55 +0000

    feat: add the notebook for updating data instead of recreating

--- launch.sh
+++ launch.sh
@@ -1,12 +1,12 @@
-nohup timeout 4h python main.py \
+nohup timeout 14h python main.py \
     --output_path data/wikipedia_synthetic.jsonl \
     --model gliznet-data \
     --api_base http://localhost:8000/v1 \
-    --num_examples 150000 \
+    --num_examples 500000 \
     --batch_size 48 \
     --max_tokens 4096 \
     --temperature 0.7 \
-    --skip 250000 \
+    --skip 556000 \
     --seed 164 \
     --shuffle_buffer 50000 &> wikipedia_synthetic.log &
 echo "ds-gen PID: $!"
